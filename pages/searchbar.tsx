81460079066f4064c05d2efc573bb24114087e88
SeeuSim <seeusimong@gmail.com>
2022-12-04 17:14:13 +0800

    tidy up tailwind styling

--- pages/searchbar.tsx
+++ pages/searchbar.tsx
@@ -18,27 +18,40 @@ export default function Searchbar() {
     <form>
       <div className="flex">
         <select id="countries" 
-                className="bg-gray-50 max-w-min border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className={`
+                  bg-gray-50 max-w-min 
+                  border border-gray-300 
+                  text-gray-900 text-sm 
+                  rounded-l-lg 
+                  focus:ring-blue-500 focus:border-blue-500 
+                  block w-full p-2.5 
+                  
+                  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                  dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                 defaultValue={"All Categories"}>
           {selectLabels.map(lbl => renderLabel(lbl))}
         </select>
-        <div id="dropdown" 
-            className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700" 
-            data-popper-reference-hidden="" 
-            data-popper-escaped="" 
-            data-popper-placement="top" 
-            style={{position: "absolute", inset: "auto auto 0px 0px", margin: "0px", transform: "translate3d(897px, 5637px, 0px)"}}>
-          <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-            {selectLabels.map(lbl => renderLabel(lbl))}
-          </ul>
-        </div>
+        
         <div className="relative w-full">
             <input type="search" 
                     id="search-dropdown" 
-                    className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
+                    className={`
+                      block p-2.5 w-full z-20 
+                      text-sm text-gray-900 
+                      bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 
+                      focus:ring-blue-500 focus:border-blue-500 
+
+                      dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 
+                      dark:text-white dark:focus:border-blue-500
+                    `}
                     placeholder="Search Your NFT" required/>
             <button type="submit" 
-                    className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    className={`
+                      absolute top-0 right-0 p-2.5 
+                      text-sm font-medium text-white 
+                      bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+                      dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+                    `}>
               <svg aria-hidden="true" 
                   className="w-5 h-5" 
                   fill="none" 
