a43ad7c96c2ac404f6e6b5032a6303129ebf6128
SeeuSim <seeusimong@gmail.com>
2022-12-18 16:12:12 +0800

    Remove Redis Client

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,7 +3,6 @@ import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
 
 import connectDB from "../../db/client";
-import redisClient from "../../db/connectRedis";
 import { modelsRouter } from "./models";
 
 connectDB();
@@ -12,10 +11,6 @@ export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
   model: modelsRouter,
-  testRedis: publicProcedure.query(async ({ ctx }) => {
-    const message = await redisClient.get("tRPC");
-    return { message };
-  }),
 });
 
 // export type definition of API
