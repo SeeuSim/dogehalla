7ab5bd10e3df28991e830fa82bb4fa1b56206971
SeeuSim <seeusimong@gmail.com>
2022-12-27 18:00:14 +0800

    Finalised OAuth user creation routines

--- src/pages/api/auth/callback/google.ts
+++ src/pages/api/auth/callback/google.ts
@@ -13,8 +13,9 @@ export default nextConnect()
       successRedirect: "/",
       failureRedirect: "/login"
     }),
-    (req: NextApiRequest & { user: any }, res: NextApiResponse) => {
+    (req: NextApiRequest & { user: any, session: any }, res: NextApiResponse) => {
       // you can save the user session here. to get access to authenticated user through req.user
+      req.session.user = req.user;
       res.json({ user: req.user });
       res.redirect("/");
     }
--- src/server/auth/passport.ts
+++ src/server/auth/passport.ts
@@ -5,11 +5,9 @@ import { VerifyCallback } from "passport-google-oauth2";
 import { Strategy as LocalStrategy } from "passport-local";
 
 import argon2 from "argon2";
-import session from "next-session";
+import { z } from "zod";
 
-import { trpc } from "utils/trpc";
 import { prisma } from "server/db/client";
-
 import { env } from "env/server.mjs";
 
 type GoogleOAuthSlug = {
@@ -54,7 +52,7 @@ passport.use(
       clientID: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:3000/api/auth/callback/google",
-      scope: ["email"],
+      scope: ["email", "profile"],
       passReqToCallback: true,
     },
     async (request: any, accessToken: any, refreshToken: any, slug: GoogleOAuthSlug, profile: GoogleOAuthProfile, done: VerifyCallback) => {
@@ -80,24 +78,53 @@ passport.use(
 
           if (!user) { // create
             console.log("Creating user " + profile.email);
+            
+            const user = await prisma.user.create({
+              data: {
+                name: `${profile.name.givenName} ${profile.name.familyName}`,
+                email: profile.email,
+                image: profile.picture,
+                //link: false //Signal that no OAuth is to be linked.
+              }
+            });
 
-          } else { //Check for existing profiles
-            if (!user.authprofiles) {
-              //No OAuth registered -> check for email and password
-            }
-            if (user.authprofiles.length != undefined && user.authprofiles.length > 0) {
-              //OAuth registered -> Prompt to login and link
-            }
+            const authprofile = await prisma.oAuthProfile.create({
+              data: {
+                userId: user.id,
+                created_at: (Date.now() % (2 ** 32)) as number || 0,
+                type: "",//
+                provider: profile.provider,
+                providerOAuthId: profile.id,
+                refresh_token: refreshToken,
+                access_token: accessToken,
+                expires_at: (slug.expires_in % (2 ** 32)) as number || 0,
+                token_type: slug.token_type,
+                scope: slug.scope,
+                id_token: slug.id_token,
+              }
+            });
+            done(null, user, { message: "User created successfully"});
+          } else { //User exists, prompt to login and link
+            //Check for existing profile
+
+            //Check if user.link -> create oauth profile and link
+
+            //Else
+            done(null, false, { message: "User with this email already exists. Please login and link your oauth provider."})
           }
+        } else {
+          const user = await prisma.user.findUniqueOrThrow({
+            where: {
+              id: authedUser.userId
+            }
+          });
+          done(null, user, { message: "User logged in successfully" });
         }
-        
-        //create session with next-session and return session, user
-        session()
-        done(null, false, {message: "logging"});
+        // //create session with next-session and return session, user -> Session to be created in api/auth/callback/google.ts
 
       } catch (err) {
         console.error(err);
-        done(err, false, { message: "Internal Server Error"});
+        done(err, false, { message: err as string });
       }
     }
   )
@@ -116,37 +143,95 @@ passport.use(
   async (accessToken: any, refreshToken: any, profile: CoinbaseProfile, done: (error: any, user?: any, info?: any) => void) => {
     //Flow is roughly the same as Google, just that this provides created_at and does not provide expires_at
     try {
-      console.log(JSON.stringify(profile._json) + `\n${accessToken}\n${refreshToken}`);
-      done(null, false, {message: "testing"})
-    } catch (err) {
-      done(err, false, { message: "Internal Server Error"});
+      const authedUser = await prisma.oAuthProfile.findUnique({
+        where: {
+          provider_providerOAuthId: {
+            provider: "coinbase",
+            providerOAuthId: profile._json.id
+          }
+        }
+      });
+
+      if (!authedUser) {
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: profile._json.email
+          }
+        });
+
+        if (!user) { //create
+          const user = await prisma.user.create({
+            data: {
+              name: profile._json.name,
+              email: profile._json.email,
+              image: profile._json.avatar_url,
+              //link: false //Signal that other OAuth profiles cannot be linked
+            }
+          });
+          const authprofile = await prisma.oAuthProfile.create({
+            data: {
+              userId: user.id,
+              created_at: new Date(profile._json.created_at).getTime() % (2 ** 32),
+              type: "",
+              provider: "coinbase",
+              providerOAuthId: profile._json.id,
+              refresh_token: refreshToken,
+              access_token: accessToken,
+              expires_at: (new Date(profile._json.created_at).getTime() + 24 * 3600 * 7) % (2 ** 32),
+              token_type: "",
+              scope: `\"wallet:user:email\", \"wallet:user:read\"`
+            }
+          });
+
+          done(null, user, { message: "User Created successfully"});
+
+        } else { //Check if can link, else error
+          //if user.link //-> Link, return user
+          //Else
+          done(null, false, { message: `User with this email address already exists. Please login to link your oauth profile.`});
+        }
+      } else {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: {
+            id: authedUser.userId
+          }
+        });
+        done(null, user, { message: "User logged in successfully" });
+      }
+
+    } catch (err) { //Error Occurred
+      done(err, false, { message: err as string});
     }
   })
 );
 
 //Local Strategy
 passport.use(new LocalStrategy(
-  async (username: string, password: string, callback: any) => {
-    const fail = callback("Error", false, { message: 'Incorrect username or password' });
+  async (username: string, password: string, callback: (error: any, user?: any, options?: { message: string }) => void) => {
     try {
-      const user = await trpc.model.user.getVerifyUser.useQuery({email: username});
-      
-      //Hashing should be transferred to login form - at later date
-      const hashedPassword = await argon2.hash(password);
-
-      if (!user || hashedPassword != user.data?.password) {
-        return fail;
-      }
-
-      return callback(null, {
-        email: user.data.email, 
-        name: user.data.name
+      //On server, interface directly with prisma -> TRPC not allowed
+      const user = await prisma.user.findUnique({
+        where: {
+          email: username
+        }
       });
-
+      
+      if (!user) {
+        // Prompt Signup via form
+        callback(null, false, { message: "Please signup via our form" });
+      } else {
+        const hashedPwd = await argon2.hash(password);
+        if (hashedPwd != user.password) {
+          callback(null, false, { message: "Password provided did not match"});
+        } else {
+          callback(null, user, { message: "User logged in successfully"});
+        }
+      }
     } catch (err) {
       //Error logging should be turned off in production
       //return fail;
-      return callback(err, false, { message: err });
+      callback(err, false, { message: err as string });
     }
   }
 ))
