d44c1b7c2bacec797977e8de4100208d062aab34
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-01-10 01:38:26 +0800

    Updated banner component

--- src/pages/analytics/(analytics-components)/banner.tsx
+++ src/pages/analytics/(analytics-components)/banner.tsx
@@ -1,10 +1,16 @@
-const Banner: React.FC<{imageUrl: string}> = ({imageUrl}) =>{
-  return (
-  <img
-    src= {imageUrl}
-    alt= "NFTBannerImage"
-  />
-  )
+import * as React from 'react';
+
+interface Props {
+  bannerImageUrl: string; 
+  logoImageUrl: string; 
 }
+
+const Banner: React.FC<Props> = ({ bannerImageUrl, logoImageUrl }) => {
+  return (
+    <div className="banner" style={{ backgroundImage: `url(${bannerImageUrl})`, backgroundSize: 'cover' }}>
+      <div className="logo" style={{ backgroundImage: `url(${logoImageUrl})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} />
+    </div>
+  );
+};
 
 export default Banner;
