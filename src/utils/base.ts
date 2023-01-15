fdd0087e530a3922ce6d8f617c6e5fddb74000b0
SeeuSim <seeusimong@gmail.com>
2023-01-15 10:34:27 +0800

    Fix redirects for deployment

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -1,4 +1,4 @@
 // Fix for windows, should be reverted for production
-export const BASEURL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+export const BASEURL = process.env.BASE
+  ? `https://${process.env.BASE}` // SSR should use vercel url
   : `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
